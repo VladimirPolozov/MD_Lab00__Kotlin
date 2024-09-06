b22dd6ac9748cbfe5a806faf099f527e0c35b486
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-09-06 17:26:56 +0700

    Added first function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
 package org.example
 
 
+fun getSumOfFirstAndLastDigits(number: String) : Int {
+    val firstDigit  = number.first().digitToInt()
+    val lastDigit = number.last().digitToInt()
+    return firstDigit + lastDigit
+}
+
 fun main() {
-    print("")
+    print("Введите целое положительное число: ")
+    var number = readln()
+    println(getSumOfFirstAndLastDigits(number))
 }
